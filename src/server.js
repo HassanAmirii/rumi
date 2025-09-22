bb49e20fda374bac1486998d3f757c797b60a092
Hassan Amiri <hassanamiri.ai@gmail.com>
2025-09-22 12:15:33 +0100

    feat: implement basic CRUD routes and server setup

--- src/server.js
+++ src/server.js
@@ -1,2 +1,13 @@
+require("dotenv").config();
 const { prisma } = require("./middleware/db");
 const app = require("./middleware/app");
+const port = process.env.PORT;
+
+app.post("/new", (req, res) => {});
+app.get("/posts", (req, res) => {});
+app.patch("/update", (req, res) => {});
+app.delete("/remove", (req, res) => {});
+
+app.listen(port, () => {
+  console.log(` app listening on localhost:${port}`);
+});
